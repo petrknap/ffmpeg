1dbc15970ca1c83f1850e88aec1ae87f43b0bb86
Petr Knap <8299754+petrknap@users.noreply.github.com>
2023-07-12 19:52:54 +0000

    fix: escaped TRF file path

--- mp4_vidstab.bat
+++ mp4_vidstab.bat
@@ -10,6 +10,6 @@ call %dir%\priority_low.bat ffmpeg
 
 %dir%\ffmpeg.exe -i "%file%" -vf "[in] vidstabtransform=smoothing=30:zoom=5:input='%trf_file%' [out]" -acodec copy "%file%_vidstab.mp4"
 
-del %trf_file%
+del "%trf_file%"
 
 call %dir%\mp4_sidebyside.bat "%file%" "%file%_vidstab.mp4" "%file%_vidstab_comparison.mp4"
